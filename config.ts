7cec76f20c56a016e66584a6101beb04e96b71af
claudebarde <lotolosenc.oc@gmail.com>
2023-01-05 12:13:41 +0100

    Wrote code for contracts origination

--- config.ts
+++ config.ts
@@ -4,6 +4,7 @@ export default {
     sk: "edskRpm2mUhvoUjHjXgMoDRxMKhtKfww1ixmWiHCWhHuMEEbGzdnz8Ks4vgarKDtxok7HmrEo1JzkXkdkvyw7Rtw6BNtSd7MJ7"
   },
   network: {
-    ghostnet: "https://ghostnet.ecadinfra.com"
+    ghostnet: "https://ghostnet.ecadinfra.com",
+    flextesa: "http://localhost:20000"
   }
 };
--- originate-contracts.ts
+++ originate-contracts.ts
@@ -1,33 +1,73 @@
-import { TezosToolkit } from "@taquito/taquito";
+import { TezosToolkit, OpKind } from "@taquito/taquito";
 import { InMemorySigner } from "@taquito/signer";
 import config from "./config";
 import fs from "fs";
 
-export default async (): Promise<
-  Array<{ contract: string; address: string }>
-> => {
+interface OriginResult {
+  name: string;
+  address: string;
+}
+
+export default async (): Promise<Array<OriginResult>> => {
   if (!fs.existsSync("./contracts.json")) {
     console.log("Originating the contracts");
     // setting up the environment
-    const Tezos = new TezosToolkit(config.network.ghostnet);
+    const Tezos = new TezosToolkit(config.network.flextesa);
     const signer = new InMemorySigner(config.alice.sk);
     Tezos.setSignerProvider(signer);
-    // fetching the contract file names
-    const fileNames = fs.readdirSync("./test-contracts");
-    // fetching the contracts code
-    const files = fileNames
-      .map(name => ({
+    try {
+      // fetching the contract file names
+      const fileNames = fs.readdirSync("./test-contracts");
+      // fetching the contracts code
+      const files = fileNames.map(name => ({
         name,
-        code: fs.readFileSync(`./test-contracts/${name}`, { encoding: "ascii" })
-      }))
-      .map(file => ({
-        ...file,
-        code: file.code.replaceAll("\n", "").replaceAll("  ", "")
+        code: fs
+          .readFileSync(`./test-contracts/${name}`, { encoding: "ascii" })
+          .toString()
+          .replace(/\n/g, "")
+          .replace(/\s\s+/g, " ")
       }));
-    console.log(files);
-    // originating the contracts
+      // originating the contracts
+      const initialStorage = "taquito";
+      const batch = await Tezos.contract.batch(
+        files.map(f => ({
+          kind: OpKind.ORIGINATION,
+          balance: 0,
+          code: f.code,
+          storage: initialStorage
+        }))
+      );
+      const batchOp = await batch.send();
+      await batchOp.confirmation();
 
-    return [];
+      let contracts: Array<OriginResult> = batchOp.results
+        .map((res, index) => {
+          const arr = (res as any)?.metadata?.operation_result
+            ?.originated_contracts;
+
+          if (
+            arr &&
+            Array.isArray(arr) &&
+            arr.length === 1 &&
+            typeof arr[0] === "string" &&
+            arr[0].slice(0, 3) === "KT1"
+          ) {
+            return {
+              name: files[index].name,
+              address: arr[0]
+            };
+          } else {
+            return undefined;
+          }
+        })
+        .filter((el): el is OriginResult => typeof el !== undefined);
+
+      console.log("Origination of contracts confirmed!");
+
+      return contracts;
+    } catch (error) {
+      return Promise.reject(JSON.stringify(error));
+    }
   } else {
     return Promise.reject("Contracts list already exists");
   }
